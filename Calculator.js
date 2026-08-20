a92b1b43a91a2e3f2a12a57defbe71159037c22c
Sneha <mailmesneha2005@gmail.com>
2026-08-20 22:38:20 +0530

    Initial commit

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+const calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    sub: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+};
+console.log(calculator.add(10, 5));
+console.log(calculator.sub(10, 5));
+console.log(calculator.multiply(10, 5));
+console.log(calculator.divide(10, 5));
